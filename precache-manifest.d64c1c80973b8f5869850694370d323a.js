45a19879ae5823f7edabd8ec8713b1e5a91e674f
uwusto <toloza.augusto@gmail.com>
2019-08-04 20:01:50 -0300

    Updates

---
+++ precache-manifest.d64c1c80973b8f5869850694370d323a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfeee241971ca098cadd0fbf23406cf",
+    "url": "/react-mon/index.html"
+  },
+  {
+    "revision": "0bf23ff832a030268bd4",
+    "url": "/react-mon/static/css/main.17efb666.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-mon/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0bf23ff832a030268bd4",
+    "url": "/react-mon/static/js/main.6042dcbb.chunk.js"
+  },
+  {
+    "revision": "8ab036abb6e0831831bd",
+    "url": "/react-mon/static/js/runtime~main.93b2e6d6.js"
+  }
+]);
